8ce28a61bdc7edbf40b675d2a00b9ae990622fc0
unknown <yaosobolev@gmail.com>
2024-04-21 12:59:27 +0500

    achievement

--- src/components/AchievementBlock.tsx
+++ src/components/AchievementBlock.tsx
@@ -4,52 +4,32 @@ import { FaHeart, FaRegHeart, FaStar } from "react-icons/fa";
 import { Link } from "react-router-dom";
 import { User } from "../pages/Rating";
 
-type UserProps = {
-  user: User;
+type AchievementBlockProps = {
+  id: number;
+  title: string;
+  date: string;
+  type: number;
 };
 
-export const AchievementBlock: React.FC<UserProps> = ({ user }) => {
-  const { id, firstname, lastname, group, isFavorite } = user;
-
-  const [favorite, setFavorite] = React.useState(isFavorite);
-
-  const handleFavorite = () => {
-    setFavorite(!favorite);
-  };
-
-  const shortGroupName = group?.flow?.department?.faculty?.name
-    .split(" ")
-    .map((el) => el[0].toUpperCase())
-    .join("");
+export const AchievementBlock: React.FC<AchievementBlockProps> = ({
+  event,
+}) => {
+  const { id, title, date, type } = event;
 
   return (
     <div className="block max-w-full  bg-white rounded-2xl">
       <div className="flex justify-between items-center py-2 px-4 ">
         <div className="flex items-center">
           <span className="mr-4">{id + 1}</span>
-          <Link to={`/profile/${id}`} className="text-black hover:underline">
-            {firstname + " " + lastname}
-          </Link>
         </div>
-        <Link
-          to={`/faculty/${group?.flow?.department?.faculty.id}`}
-          className="text-black hover:underline mr-4"
-        >
-          {shortGroupName}
-        </Link>
-        <Link
-          to={`/group/${group?.id}`}
-          className="text-black hover:underline mr-4"
-        >
-          {group?.flow?.name + "-" + group?.name}
-        </Link>
+        <div>{title}</div>
+
         <div className="flex gap-x-1 items-center mr-4">
-          <FaStar className="text-yellow-300" />
-          <span className="block">{1}</span>
+          <span className="block">{date}</span>
         </div>
-        <button onClick={() => handleFavorite()} className={`  `}>
-          {favorite ? <FaHeart className="text-red-500" /> : <FaRegHeart />}
-        </button>
+        <div className="flex gap-x-1 items-center mr-4">
+          <span className="block">{type}</span>
+        </div>
       </div>
     </div>
   );
--- src/pages/achievements/index.tsx
+++ src/pages/achievements/index.tsx
@@ -10,16 +10,64 @@ const Achievement = () => {
     return segments[2];
   };
   console.log(pathname);
-  const usersData = [1, 2, 3];
+  const events = [
+    {
+      id: 0,
+      title: "a",
+      date: "12-12-1212",
+      type: 0,
+    },
+    {
+      id: 1,
+      title: "b",
+      date: "12-12-1212",
+      type: 1,
+    },
+    {
+      id: 2,
+      title: "c",
+      date: "12-12-1212",
+      type: 2,
+    },
+    {
+      id: 3,
+      title: "c",
+      date: "12-12-1212",
+      type: 3,
+    },
+    {
+      id: 4,
+      title: "d",
+      date: "12-12-1212",
+      type: 2,
+    },
+  ];
+
+  const filterEventsByType = (events, type) => {
+    return events.filter((event) => event.type === type);
+  };
+
   const path = removeSlashes(pathname);
+
+  const translateType = () => {
+    if (path === "science") {
+      return 0;
+    } else if (path === "creativity") {
+      return 1;
+    } else if (path === "sport") {
+      return 2;
+    } else return 3;
+  };
+  const achievementList = filterEventsByType(events, translateType());
+
   return (
     <div className="flex flex-row justify-between gap-x-4">
       <div className="flex flex-col flex-grow gap-y-3 overflow-y-auto">
         <div className="block max-w-full  bg-white rounded-2xl ">
           <h2 className=" p-4 mb-4 text-center text-4xl">Список достижений</h2>
         </div>
-        {usersData.map((user, index) => (
-          <AchievementBlock key={user?.id || index} user={user} />
+        {achievementList.map((event, index) => (
+          <AchievementBlock key={event?.id || index} event={event} />
         ))}
       </div>
     </div>
