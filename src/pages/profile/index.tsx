8fdc564ad47cb8355319b83ca45e9ad957f28dcc
lorezv <dima.derkach2004@gmail.com>
2024-04-21 20:57:21 +0500

    fix

--- src/pages/profile/index.tsx
+++ src/pages/profile/index.tsx
@@ -1,15 +1,24 @@
 import React, { useEffect, useState } from "react";
 import axios from "../../core/";
 import { useParams } from "react-router-dom";
-import { IUser, IUserAvatar } from "../../interface/models/user";
+import {
+  IUser,
+  IUserAvatar,
+  IUniversity,
+  IUniversityAvatar,
+} from "../../interface/models";
 
 export default function () {
   const { id } = useParams();
   if (!id) {
     throw "ID_REQUIRED_IN_URL";
   }
-  const [user, setUser] = useState<IUser | null>(null);
+  const [user, setUser] = useState<
+    (IUser & { university: IUniversity }) | null
+  >(null);
   const [avatar, setAvatar] = useState<IUserAvatar | null>(null);
+  const [universityAvatar, setUniversityAvatar] =
+    useState<IUniversityAvatar | null>(null);
 
   async function fetchUser() {
     const response = await axios.get(`/users/${id}`);
@@ -28,6 +37,13 @@ export default function () {
     }
   }
 
+  async function fetchUniversityAvatar() {
+    const response = await axios.get(`/university/${id}/avatar`);
+    if (response.data) {
+      setUniversityAvatar(response.data.data);
+    }
+  }
+
   async function editUser(data: { content?: string }) {
     const response = await axios.patch(`/users/${id}/`, data);
     if (response.data.data) {
@@ -40,43 +56,79 @@ export default function () {
     fetchUserAvatar();
   }, []);
 
+  useEffect(() => {
+    if (user) {
+      fetchUniversityAvatar();
+    }
+  }, [user]);
+
   function RenderUserCard(user: IUser) {
     return (
-      <div className="flex gap-8">
+      <div className="bg-white rounded p-6 flex gap-4 shadow-md shadow-zinc-200 border border-zinc-200">
+        <div>
+          <img
+            className="w-28 h-28 rounded-full"
+            src={
+              !avatar
+                ? "http://localhost:3000/images/default-user-avatar.jpg"
+                : `http://localhost:3000/images/${avatar.path}`
+            }
+          />
+        </div>
         <div className="flex-1 flex flex-col">
-          <div className="bg-white rounded p-6 flex gap-4 shadow-md shadow-zinc-200 border border-zinc-200">
-            <div>
-              <img
-                className="w-28 h-28 rounded-full"
-                src={
-                  !avatar
-                    ? "http://localhost:3000/images/default-user-avatar.jpg"
-                    : `http://localhost:3000/images/${avatar.path}`
-                }
-              />
-            </div>
-            <div className="flex-1 flex flex-col">
-              <div>
-                <h2 className="text-lg font-medium">{`${user.firstname} ${user.lastname}`}</h2>
-                <p className="text-sm text-zinc-500">
-                  {user.type == "STUDENT" ? "Студент" : "Представитель ВУЗа"}
-                </p>
-              </div>
-              <div className="flex">
-                <textarea
-                  className="w-full resize-none border-none"
-                  value={user.content}
-                  onInput={(e) => setUser({ ...user, content: e.target.value })}
-                  onBlur={(e) => editUser({ content: e.target.value })}
-                  placeholder="Напшите немного о себе..."
-                ></textarea>
-              </div>
-            </div>
+          <div>
+            <h2 className="text-lg font-medium">{`${user.firstname} ${user.lastname}`}</h2>
+            <p className="text-sm text-zinc-500">
+              {user.type == "STUDENT" ? "Студент" : "Представитель ВУЗа"}
+            </p>
+          </div>
+          <div className="flex">
+            <textarea
+              className="w-full resize-none border-none"
+              value={user.content}
+              onInput={(e) => setUser({ ...user, content: e.target.value })}
+              onBlur={(e) => editUser({ content: e.target.value })}
+              placeholder="Напшите немного о себе..."
+            ></textarea>
           </div>
         </div>
       </div>
     );
   }
 
-  return <>{user ? RenderUserCard(user) : ""}</>;
+  function RenderDelegateUniversity(user: IUser) {
+    return (
+      <>
+        <div className="bg-white rounded p-6 shadow-md shadow-zinc-200 border border-zinc-200">
+          <div className="flex gap-4">
+            <div>
+              <img
+                className="w-28 h-28 rounded-full"
+                src={
+                  !universityAvatar
+                    ? "http://localhost:3000/images/default-university-avatar.jpg"
+                    : `http://localhost:3000/images/${universityAvatar.path}`
+                }
+              />
+            </div>
+            <div className="flex-1 flex flex-col">
+              <h2 className="text-lg font-medium">{user.university.name}</h2>
+              <p className="text-sm text-zinc-500">{user.university.content}</p>
+            </div>
+          </div>
+        </div>
+      </>
+    );
+  }
+
+  return (
+    <>
+      <div className="flex gap-8">
+        <div className="flex-1 flex flex-col gap-8">
+          {user && RenderUserCard(user)}
+          {user && user.type == "DELEGATE" && RenderDelegateUniversity(user)}
+        </div>
+      </div>
+    </>
+  );
 }
